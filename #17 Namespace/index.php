244094736b0da7ba99596aea0f49e7cc43bd2cef
Ilham-Bashthotan <ilhambashthotan123@gmail.com>
2025-01-18 20:37:19 +0700

    #17

---
+++ #17 Namespace/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'App/init.php';
+
+// $produk1 = new Komik("Jinapa", "Gitana Sari", "fafa", 2000, 20);
+// $produk2 = new Game("Gevasiga", "Guna Tama", "fufu", 25000, 21);
+
+
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetak();
+
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
+
+new ServiceUser();
+new ProdukUser();
